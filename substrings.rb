fb95000e30aad14793435e0d4bfed39076eee2a6
Steven Parker <sparker81@gmail.com>
2018-12-19 22:11:57 -0500

    started substrings method

---
+++ substrings.rb
@@ -0,0 +1,8 @@
+# Takes a word and a dictionary and returns a hash containing the
+# words found and how many times it was found.
+# @param word String word to find in dictionary
+# @param dictionary Array<String> list of words for the dictionary
+# @return Hash list of words found from dictionary
+def substring(word, dictionary)
+
+end
